2f848c4d599608d455b6f9ca0edc228b4e75cc6e
xp9477 <947760133@qq.com>
2025-06-08 18:43:48 +0800

    更新打印机配置为Canon PIXMA TS3380，移除不必要的设置

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PTR="Brother-MFC-L2710DW"
+PTR="Canon_PIXMA_TS3380"
 
 service cups start
 
 echo "Configuring the printer $PTR."
-lpadmin -p $PTR -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_opt.ppd -o printer-is-shared=true -o printer-info="Brother MFC-L2710DW" -o printer-location="Home Office" -o printer-make-and-model="Brother MFC-L2710DW" -o printer-state=3 -o printer-state-reasons=none -o printer-state-message="Ready to print"
+lpadmin -p $PTR -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_opt.ppd
 
 echo "Setting the default printer to $PTR."
 lpadmin -d $PTR
