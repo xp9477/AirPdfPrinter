f60399269370a29a641e0262769ad2f2f8a7e82a
xp9477 <947760133@qq.com>
2025-06-08 18:28:08 +0800

    更新打印机配置为Brother MFC-L2710DW, Github Action编译

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PTR="PDF"
+PTR="Brother-MFC-L2710DW"
 
 service cups start
 
 echo "Configuring the printer $PTR."
-lpadmin -p $PTR -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_opt.ppd
+lpadmin -p $PTR -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_opt.ppd -o printer-is-shared=true -o printer-info="Brother MFC-L2710DW" -o printer-location="Home Office" -o printer-make-and-model="Brother MFC-L2710DW" -o printer-state=3 -o printer-state-reasons=none -o printer-state-message="Ready to print"
 
 echo "Setting the default printer to $PTR."
 lpadmin -d $PTR
